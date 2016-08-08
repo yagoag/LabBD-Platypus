d8f7f96a7cdc0a38a14b98766b7b1656984f10f8
Yago Arroyo <yagoag@gmail.com>
2016-08-08 09:12:27 -0300

    Adicionados os procedures restantes para as Licencas

--- proceduresUnificados.sql
+++ proceduresUnificados.sql
@@ -14,3 +14,34 @@ BEGIN
     END
 END
 GO
+
+CREATE PROCEDURE apagaLicenca (@siape char(9), @dataInicio date, @dataTermino date)
+AS
+BEGIN
+    DECLARE @licencaExiste char(9);
+    
+	SELECT @licencaExiste = siape FROM Licenca WHERE siape = @siape AND dataInicio = @dataInicio AND dataTermino = @dataTermino;
+	IF @licencaExiste IS NOT NULL
+    BEGIN
+        DELETE FROM Licenca WHERE siape = @siape AND dataInicio = @dataInicio AND dataTermino = @dataTermino;
+    END
+END
+GO
+
+CREATE PROCEDURE atualizaLicenca (@siape char(9), @dataInicio date, @dataTermino date, @documento varchar(500), @indicacao varchar(200), @motivo varchar(200))
+AS
+BEGIN
+    DECLARE @licencaExiste char(9);
+    
+	SELECT @licencaExiste = siape FROM Licenca WHERE siape = @siape AND dataInicio = @dataInicio AND dataTermino = @dataTermino;
+	IF @licencaExiste IS NOT NULL
+	BEGIN
+		IF @motivo IS NOT NULL AND @motivo != ''
+		BEGIN
+			UPDATE Licenca
+				SET dataInicio = @dataInicio, dataTermino = @dataTermino, documento = @documento, indicacao = @indicacao, motivo = @motivo
+				WHERE siape = @siape AND dataInicio = @dataInicio AND dataTermino = @dataTermino;
+		END
+	END
+END
+GO
