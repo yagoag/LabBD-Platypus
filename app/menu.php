45194355c3c4168966a0adf169948929ab161a38
Yago Arroyo <yagoag@gmail.com>
2016-09-14 03:54:06 -0300

    Adição das páginas ao menu

--- app/menu.php
+++ app/menu.php
@@ -11,7 +11,8 @@
 
   // Verifica se é funcionário
   $stmt = sqlsrv_query($conn, 'SELECT siape FROM Funcionario WHERE cpf = '.$_SESSION['cpf']);
-  if (sqlsrv_has_rows($stmt)) {
+  $_SESSION['papeis']['funcionario'] = sqlsrv_has_rows($stmt);
+  if ($_SESSION['papeis']['funcionario']) {
     $_SESSION['siape'] = sqlsrv_fetch_array($stmt)['siape'];
     
     // Verifica se é técnico
@@ -23,10 +24,10 @@
     $_SESSION['papeis']['docente']  = sqlsrv_has_rows($stmt);
     
     // Verifica se é Chefe de Departamento
-    $stmt = sqlsrv_query($conn, "SELECT siape FROM DocenteTemCargoAdministrativo WHERE siape = ".$_SESSION['siape']." AND nomeCargo = 'Chefe de Departamento'");
+    $stmt = sqlsrv_query($conn, "SELECT siape FROM DocenteTemCargoAdministrativo WHERE siape = '".$_SESSION['siape']."' AND nomeCargo = 'Chefe de Departamento'");
     $_SESSION['papeis']['chefedepto']  = sqlsrv_has_rows($stmt);
     
-    $stmt = sqlsrv_query($conn, "SELECT siape FROM DocenteTemCargoAdministrativo WHERE siape = ".$_SESSION['siape']." AND nomeCargo = 'Coordenador de Curso'");
+    $stmt = sqlsrv_query($conn, "SELECT siape FROM DocenteTemCargoAdministrativo WHERE siape = '".$_SESSION['siape']."' AND nomeCargo = 'Coordenador de Curso'");
     $_SESSION['papeis']['coordcurso']  = sqlsrv_has_rows($stmt);
   } else {
       $_SESSION['papeis']['tecnico']  = false;
@@ -36,6 +37,7 @@
   }
 ?>
 <div class="pure-menu">
+    <div class="main-heading"><a href="index.php">Sair</a></div>
     <ul class="pure-menu-list">
       <?php
         if ($_SESSION['papeis']['aluno']) {
@@ -53,11 +55,11 @@
         }
         if ($_SESSION['papeis']['tecnico']) {
       ?>
-        <div class="pure-menu-heading">Tecnico</div>
+        <div class="pure-menu-heading">Técnico</div>
 
         <ul class="pure-menu-list">
             <li class="pure-menu-item"><a href="InfoTecPessoais.php" class="pure-menu-link">Informações Pessoais</a></li>
-            <li class="pure-menu-item"><a href="licencas.php" class="pure-menu-link">Licenças</a></li>
+            <li class="pure-menu-item"><a href="equipesApoio.php" class="pure-menu-link">Equipes de Apoio</a></li>
         </ul>
       <?php
         }
@@ -68,7 +70,11 @@
 
         <ul class="pure-menu-list">
             <li class="pure-menu-item"><a href="informacaoPessoalDocente.php" class="pure-menu-link">Informação Pessoal</a></li>
-            <li class="pure-menu-item"><a href="licencas.php" class="pure-menu-link">Licenças</a></li>
+            <li class="pure-menu-item"><a href="planodeensinodocente.php" class="pure-menu-link">Plano de Ensino</a></li>
+            <li class="pure-menu-item"><a href="prioridade.php" class="pure-menu-link">Prioridades</a></li>
+            <li class="pure-menu-item"><a href="comunicadoDocente.php" class="pure-menu-link">Comunicados</a></li>
+            <li class="pure-menu-item"><a href="IntervencaoDocente.php" class="pure-menu-link">Intervenções</a></li>
+            
         </ul>
       <?php
         }
@@ -85,7 +91,23 @@
       
         if ($_SESSION['papeis']['coordcurso']) {
       ?>
+        <div class="pure-menu-heading">Coordenador de Curso</div>
+
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item"><a href="itemdePautaRCC.php" class="pure-menu-link">Itens de Pauta</a></li>
+            <li class="pure-menu-item"><a href="projetoPoliticoPedagogico.php" class="pure-menu-link">Projeto Político Pedagógico</a></li>
+          <li class="pure-menu-item"><a href="situacaoEnade.php" class="pure-menu-link">Situação Enade</a></li>
+        </ul>
+      <?php
+        }
       
+        if ($_SESSION['papeis']['funcionario']) {
+      ?>
+        <div class="pure-menu-heading">Funcionário</div>
+
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item"><a href="licencasFuncionarios.php" class="pure-menu-link">Minhas Licenças</a></li>
+        </ul>
       <?php
         }
       ?>
