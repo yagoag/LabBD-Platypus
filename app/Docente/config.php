e4eae19f706542e14908e562356a45ab6cf04ce6
Yago Arroyo <yagoag@gmail.com>
2016-09-14 17:33:31 -0300

    Remoção de itens duplicados, mantendo apenas os unificados.

--- app/Docente/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server = 'DESKTOP-0ITTBBB\SQLEXPRESS';
-$connectionInfo = array("Database" => "LabBD", "CharacterSet" => "UTF-8");
-// Connect to MSSQL
-$conn = sqlsrv_connect($server, $connectionInfo);
-if (!$conn) {
-    die('A conexao com SQL_SERVER nao funcionou corretamente.');
-}
-?>
